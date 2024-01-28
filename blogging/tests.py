a0a1d802a114d59bd7b9cf8badb69efc5630c551
marlondocouto36 <marlondc@uw.edu>
2024-01-28 10:17:12 -0800

    "Completed blogging views and ran tests successfully"

--- blogging/tests.py
+++ blogging/tests.py
@@ -1,6 +1,41 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from blogging.models import Post
+import datetime
+import utc
+# needed to change it since latest version of Django doesn't seem like having timezone.utc
+
+
+class FrontEndTestCase(TestCase):
+    """test views provided in the front-end"""
+    fixtures = ['blogging_test_fixture.json', ]
+
+    def setUp(self):
+        """set up"""
+        self.now = datetime.datetime.now(datetime.UTC) # needed to change this for a timezone-aware object instead of utcnow()
+        self.timedelta = datetime.timedelta(15)
+        author = User.objects.get(pk=1)
+        for count in range(1, 11):
+            post = Post(title="Post %d Title" % count,
+                        text="foo",
+                        author=author)
+            if count < 6:
+                # publish the first five posts
+                pubdate = self.now - self.timedelta * count
+                post.published_date = pubdate
+            post.save()
+
+    def test_details_only_published(self):
+        """test"""
+        for count in range(1, 11):
+            title = "Post %d Title" % count
+            post = Post.objects.get(title=title)
+            resp = self.client.get('/posts/%d/' % post.pk)
+            if count < 6:
+                self.assertEqual(resp.status_code, 200)
+                self.assertContains(resp, title)
+            else:
+                self.assertEqual(resp.status_code, 404)
 
 
 class PostTestCase(TestCase):
--- blogging/views.py
+++ blogging/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from blogging.models import Post
+from django.template import loader
 
-# Create your views here.
+
+def stub_view(request, *args, **kwargs):
+    """Stub view"""
+    body = "Stub View\n\n"
+    if args:
+        body += "Args:\n"
+        body += "\n".join(["\t%s" % a for a in args])
+    if kwargs:
+        body += "Kwargs:\n"
+        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
+    return HttpResponse(body, content_type="text/plain")
+
+
+def list_view(request):
+    """List view"""
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    # template = loader.get_template('blogging/list.html')
+    context = {'posts': posts}
+    # body = template.render(context)
+    # return HttpResponse(body, content_type="text/html")
+    return render(request, 'blogging/list.html', context)
+
+
+def detail_view(request, post_id):
+    """shows single blog post"""
+    published = Post.objects.exclude(published_date__exact=None)
+    try:
+        post = published.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404
+    context = {'post': post}
+    return render(request, 'blogging/detail.html', context)
