b9427832b5c9bad64d65591827d24ca599af355f
marlondocouto36 <marlondc@uw.edu>
2024-02-04 13:42:54 -0800

    "Made the polling view with Django based views"

--- polling/views.py
+++ polling/views.py
@@ -1,27 +1,53 @@
 from django.shortcuts import render
-from django.http import Http404
+# from django.http import Http404
 from polling.models import Poll
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 
-def list_view(request):
-    """view of list of polls"""
-    context = {'polls': Poll.objects.all()}
-    return render(request, 'polling/list.html', context)
+class PollListView(ListView):
+    """ Django based list view"""
+    model = Poll
+    template_name = 'polling/list.html'
 
 
-def detail_view(request, poll_id):
-    """detailed view for each poll"""
-    try:
-        poll = Poll.objects.get(pk=poll_id)
-    except Poll.DoesNotExist:
-        raise Http404
+class PollDetailView(DetailView):
+    """ Django based list detail view"""
+    model = Poll
+    template_name = 'polling/detail.html'
 
-    if request.method == "POST":
+    def post(self, request, *args, **kwargs):
+        """post method within view class"""
+        poll = self.get_object()
+
         if request.POST.get("vote") == "Yes":
             poll.score += 1
         else:
             poll.score -= 1
         poll.save()
 
-    context = {'poll': poll}
-    return render(request, 'polling/detail.html', context)
+        context = {"object": poll}
+        return render(request, "polling/detail.html", context)
+
+# def list_view(request):
+#     """view of list of polls"""
+#     context = {'polls': Poll.objects.all()}
+#     return render(request, 'polling/list.html', context)
+#
+#
+# def detail_view(request, poll_id):
+#     """detailed view for each poll"""
+#     try:
+#         poll = Poll.objects.get(pk=poll_id)
+#     except Poll.DoesNotExist:
+#         raise Http404
+#
+#     if request.method == "POST":
+#         if request.POST.get("vote") == "Yes":
+#             poll.score += 1
+#         else:
+#             poll.score -= 1
+#         poll.save()
+#
+#     context = {'poll': poll}
+#     return render(request, 'polling/detail.html', context)
