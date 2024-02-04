58c43ebc6f8f57319cf679525af544c01667a520
marlondocouto36 <marlondc@uw.edu>
2024-02-04 15:53:27 -0800

    "Made the blogging view with Django based views adding queryset to it"

--- blogging/views.py
+++ blogging/views.py
@@ -1,38 +1,50 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from blogging.models import Post
-from django.template import loader
+# from django.template import loader
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+# def stub_view(request, *args, **kwargs):
+#     """Stub view"""
+#     body = "Stub View\n\n"
+#     if args:
+#         body += "Args:\n"
+#         body += "\n".join(["\t%s" % a for a in args])
+#     if kwargs:
+#         body += "Kwargs:\n"
+#         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
+#     return HttpResponse(body, content_type="text/plain")
 
 
-def stub_view(request, *args, **kwargs):
-    """Stub view"""
-    body = "Stub View\n\n"
-    if args:
-        body += "Args:\n"
-        body += "\n".join(["\t%s" % a for a in args])
-    if kwargs:
-        body += "Kwargs:\n"
-        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
-    return HttpResponse(body, content_type="text/plain")
+class PostListView(ListView):
+    """Django based list view for blog posts"""
+    queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
+    template_name = 'blogging/list.html'
 
 
-def list_view(request):
-    """List view"""
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    # template = loader.get_template('blogging/list.html')
-    context = {'posts': posts}
-    # body = template.render(context)
-    # return HttpResponse(body, content_type="text/html")
-    return render(request, 'blogging/list.html', context)
+class PostDetailView(DetailView):
+    """Django based detail view for posts:"""
+    queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
+    template_name = 'blogging/detail.html'
+
+# def list_view(request):
+#     """List view"""
+#     published = Post.objects.exclude(published_date__exact=None)
+#     posts = published.order_by('-published_date')
+#     # template = loader.get_template('blogging/list.html')
+#     context = {'posts': posts}
+#     # body = template.render(context)
+#     # return HttpResponse(body, content_type="text/html")
+#     return render(request, 'blogging/list.html', context)
 
 
-def detail_view(request, post_id):
-    """shows single blog post"""
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
+# def detail_view(request, post_id):
+#     """shows single blog post"""
+#     published = Post.objects.exclude(published_date__exact=None)
+#     try:
+#         post = published.get(pk=post_id)
+#     except Post.DoesNotExist:
+#         raise Http404
+#     context = {'post': post}
+#     return render(request, 'blogging/detail.html', context)
--- mysite/urls.py
+++ mysite/urls.py
@@ -19,8 +19,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('polling/', include('polling.urls')),
+    path('blogging/', include('blogging.urls')),
     path('admin/', admin.site.urls),
-    path('', include('blogging.urls')),
+    # path('', include('blogging.urls')),
     path('login/', LoginView.as_view(template_name='login.html'), name="login"),
     path('logout/', LogoutView.as_view(next_page='/'), name="logout"),
 ]
