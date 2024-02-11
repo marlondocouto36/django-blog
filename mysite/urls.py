2ede45aaa7cac26b0f9b6837687513e9fe551e97
marlondocouto36 <marlondc@uw.edu>
2024-02-11 14:29:19 -0800

    "Added add black to travis (2)"

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 
+
 urlpatterns = [
     path("polling/", include("polling.urls")),
     path("blogging/", include("blogging.urls")),
