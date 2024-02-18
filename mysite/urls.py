5b15fe17960271be226fb212095b7c44a07599a9
marlondocouto36 <marlondc@uw.edu>
2024-02-18 15:06:13 -0800

    "Added login via Github to local site"

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 
+# from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path("polling/", include("polling.urls")),
@@ -26,4 +28,5 @@ urlpatterns = [
     path("", include("blogging.urls")),
     path("login/", LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", LogoutView.as_view(next_page="/"), name="logout"),
+    path("accounts/", include("allauth.urls")),
 ]
